35319fb24c75a431d7daaf9c751d921e1b811309
6942x <guiwesleyferwill@gmail.com>
2026-01-02 09:28:54 -0300

    Update Auto-Rejoin.lua

--- Features/Auto-Rejoin.lua
+++ Features/Auto-Rejoin.lua
@@ -5,18 +5,20 @@ local CoreGui = game:GetService("CoreGui")
 local LocalPlayer = Players.LocalPlayer
 local Config = _G.UniversalUtility.Config
 
-_G.UniversalUtility.AutoRejoin = {}
-_G.UniversalUtility.AutoRejoin.Teleporting = false
+local AutoRejoin = {}
+_G.UniversalUtility.AutoRejoin = AutoRejoin
 
-function _G.UniversalUtility.AutoRejoin.Setup()
+AutoRejoin.Teleporting = false
+
+function AutoRejoin.Setup()
     if Config.AutoRejoinEnabled then
         local PromptGui = CoreGui:WaitForChild("RobloxPromptGui")
         local PromptOverlay = PromptGui:WaitForChild("promptOverlay")
         PromptOverlay.ChildAdded:Connect(function(child)
-            if child.Name == "ErrorPrompt" and Config.AutoRejoinEnabled and not _G.UniversalUtility.AutoRejoin.Teleporting then
-                _G.UniversalUtility.AutoRejoin.Teleporting = true
+            if child.Name == "ErrorPrompt" and Config.AutoRejoinEnabled and not AutoRejoin.Teleporting then
+                AutoRejoin.Teleporting = true
                 task.spawn(function()
-                    while Config.AutoRejoinEnabled and _G.UniversalUtility.AutoRejoin.Teleporting do
+                    while Config.AutoRejoinEnabled and AutoRejoin.Teleporting do
                         TeleportService:Teleport(game.PlaceId, LocalPlayer)
                         task.wait(2)
                     end
@@ -26,8 +28,8 @@ function _G.UniversalUtility.AutoRejoin.Setup()
     end
 end
 
-function _G.UniversalUtility.AutoRejoin.Stop()
-    _G.UniversalUtility.AutoRejoin.Teleporting = false
+function AutoRejoin.Stop()
+    AutoRejoin.Teleporting = false
 end
 
-return _G.UniversalUtility.AutoRejoin
+return AutoRejoin
